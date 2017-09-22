6463e308b089b4baf26fd2f2c619abcdd63bed51
wayshall <weishao.zeng@gmail.com>
2017-09-22 19:28:35 +0800

    bugfix

--- src/main/java/org/onetwo4j/sample/WechatStarter.java
+++ src/main/java/org/onetwo4j/sample/WechatStarter.java
@@ -4,11 +4,11 @@ import javax.annotation.PostConstruct;
 
 import org.onetwo.boot.core.EnableJFishBootExtension;
 import org.onetwo.boot.core.EnableJFishBootExtension.AppcationType;
+import org.onetwo.dbm.spring.EnableDbm;
 import org.onetwo.ext.apiclient.wechat.EnableWechatClient;
 import org.onetwo.ext.apiclient.wechat.serve.dto.ReceiveMessage.TextMessage;
 import org.onetwo.ext.apiclient.wechat.serve.dto.ReplyMessage.TextReplyMessage;
 import org.onetwo.ext.apiclient.wechat.serve.spi.MessageRouterService;
-import org.onetwo.ext.apiclient.wechat.serve.spi.WechatSessionRepository;
 import org.onetwo.ext.apiclient.wechat.utils.WechatConstants.MessageType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @SpringBootApplication
 @EnableWechatClient
+@EnableDbm
 @EnableJFishBootExtension(appcationType=AppcationType.WEB_UI)
 public class WechatStarter {
 	
@@ -37,17 +38,10 @@ public class WechatStarter {
 	}
 	
 	@RequestMapping("/")
-	public String home() {
-        return "index";
+	public String home(/*OAuth2UserInfo userInfo*/) {
+        return "forward:index.html";
     }
 	
-	@RequestMapping("/payment")
-	public String payment(boolean wechatBinded) {
-		if(wechatBinded){
-			return "payment";
-		}
-        return "wechat-bind";
-    }
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(WechatStarter.class)
